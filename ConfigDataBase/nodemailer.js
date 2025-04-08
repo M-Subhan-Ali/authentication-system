8f302bed0845b9432606dca389832b755ebab055
M-Subhan-Ali <s8289552@gmail.com>
2025-04-08 17:42:46 +0500

    otp send Successfully

--- ConfigDataBase/nodemailer.js
+++ ConfigDataBase/nodemailer.js
@@ -1,28 +1,82 @@
-// utils/sendEmail.js
 import nodemailer from "nodemailer";
 
-const sendWelcomeEmail = async (toEmail, name) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+const sendWelcomeEmail = async ( name , toEmail ) => {
 
-    const mailOptions = {
-      from: `"MERN Auth" <${process.env.EMAIL_USER}>`,
-      to: toEmail,
-      subject: "🎉 Welcome to MERN Auth!",
-      html: `<h3>Hi ${name},</h3><p>Thanks for registering with our app!</p>`,
-    };
+ try {
+   const transporter = nodemailer.createTransport({
+     service : "Gmail",
+     auth:{
+       user : process.env.EMAIL_USER,
+       pass : process.env.EMAIL_PASS
+     }
+   })
+ 
+   const mailOptions = {
+          from: `"MERN Auth" <${process.env.EMAIL_USER}>`,
+          to: toEmail,
+          subject: "🎉 Welcome to MERN Auth!",
+          html: `<h3>Hi ${name},</h3><p>Thanks for registering with our app!</p>`,
+        };
+ 
+   await transporter.sendMail(mailOptions)
 
-    await transporter.sendMail(mailOptions);
-    console.log("✅ Welcome email sent to:", toEmail);
-  } catch (error) {
-    console.error("❌ Failed to send welcome email:", error);
+   console.log(`Email was Sent to ${toEmail}`)
+ 
+ } catch (error) {
+  console.log("❌ Failed to send Welcome Email",error)
+ }
+
+}
+
+export const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth : {
+    user : process.env.EMAIL_USER ,
+    pass : process.env.EMAIL_PASS
   }
-};
+})
 
-export default sendWelcomeEmail;
+export {sendWelcomeEmail }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import nodemailer from "nodemailer";
+
+// const sendWelcomeEmail = async (toEmail, name) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       service: "Gmail",
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
+
+//     const mailOptions = {
+//       from: `"MERN Auth" <${process.env.EMAIL_USER}>`,
+//       to: toEmail,
+//       subject: "🎉 Welcome to MERN Auth!",
+//       html: `<h3>Hi ${name},</h3><p>Thanks for registering with our app!</p>`,
+//     };
+
+//     await transporter.sendMail(mailOptions);
+//     console.log("✅ Welcome email sent to:", toEmail);
+//   } catch (error) {
+//     console.error("❌ Failed to send welcome email:", error);
+//   }
+// };
+
+// export default sendWelcomeEmail;
--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,7 +1,8 @@
 import { User } from "../models/user.models.js";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken" 
-import sendWelcomeEmail from "../ConfigDataBase/nodemailer.js";
+// import { sendWelcomeEmail } from "../ConfigDataBase/nodemailer.js";
+import { transporter } from "../ConfigDataBase/nodemailer.js";
 
 export const register = async ( req , res ) => {
   
@@ -44,8 +45,17 @@ export const register = async ( req , res ) => {
       sameSite :  process.env.NODE_ENV === "production" ? 'none' : 'strict',
       maxAge : 9 * 24 * 60 * 60 * 1000
     })
+
+    const mailOptions = {
+      from:`Mern Authentication App ${process.env.EMAIL_USER}`,
+      to: email,
+      subject: "Welcome to Mern Authentication App",
+      text: `Hello ${name} , Welcome to Mern Authentication App`
+    }
     
-    await sendWelcomeEmail(email,name)
+    // await sendWelcomeEmail(name,email)
+
+    await transporter.sendMail(mailOptions)
 
     return res.json({
       success : true,
@@ -133,4 +143,60 @@ export const Logout = async ( req , res ) => {
     
   }
   
+}
+
+export const sendVerifyOTP = async ( req ,res ) => {
+ 
+  //steps for my learning
+  //get userid from frontend 
+  //find user from db
+  //check if its verify or not
+  //if verify then show its verify
+  //if not verify then  create otp and set expireotp and save it in user
+  //otp send to the email
+  
+
+ try {
+   const { userID } = req.body ;
+ 
+   const user = await User.findById(userID);
+ 
+   if( !user ){
+     return res.status(404).json({message : "User Not Found!"})
+   }
+ 
+   if( user.verifyOTP ){
+     return res.json({
+       success : false, //because the user is already verify why he again verify
+       message : "User Already Verified!"})
+   }
+ 
+ 
+   const OTP = String( Math.floor( 100000 + Math.random() * 900000 ) );
+
+   user.verifyOTP_expire = Date.now() + 24 * 60 * 60 * 1000;
+
+   await user.save();
+ 
+   const mailOptions = {
+     from : `OTP ${process.env.EMAIL_USER}`,
+     to : user.email,
+     subject : "Verify Your Account",
+     html : `<h1>Your OTP is ${OTP} </h1>`,
+   }
+ 
+   await transporter.sendMail(mailOptions)
+   
+   return res.status(201).json({
+     success : true , 
+     message : "Verification OTP Send Successfully"
+   })
+
+ } catch (error) {
+  res.status(500).json({
+    success : false ,
+    message : `internal server error ${error}`
+  })
+ }
+
 }
