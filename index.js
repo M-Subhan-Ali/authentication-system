7c1cd32b740983da5b890236443ab7c44a01ba0e
M-Subhan-Ali <s8289552@gmail.com>
2025-04-26 23:11:48 +0500

    updating

--- index.js
+++ index.js
@@ -11,29 +11,34 @@ const port = process.env.PORT || 4000;
 
 DBConnect();
 
-const allowedOrigin = [ "http://localhost:5173" , "https://authentication-mern-app-gold.vercel.app" , "https://*.vercel.app" ] 
+// const allowedOrigin = [  "https://authentication-mern-app-gold.vercel.app"  ] 
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({origin : allowedOrigin , credentials: true}))
-// app.use(cors({origin : allowedOrigin , credentials: true}))
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigin.some(allowed => {
-      return origin === allowed || 
-             origin.endsWith(allowed.replace('https://', ''))
-    })) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  exposedHeaders: ['set-cookie'] // If using cookies
+  origin: 'https://authentication-mern-app-gold.vercel.app',
+  methods: ['GET', 'POST' , 'PUT', 'DELETE' , 'PATCH'],
+  credentials: true 
 }));
+// app.use(cors({origin : allowedOrigin , credentials: true}))
+// app.use(cors({origin : allowedOrigin , credentials: true}))
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
+    
+//     if (allowedOrigin.some(allowed => {
+//       return origin === allowed || 
+//              origin.endsWith(allowed.replace('https://', ''))
+//     })) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   exposedHeaders: ['set-cookie'] // If using cookies
+// }));
 
 
 app.get("/hehe" , (req , res)=>{
