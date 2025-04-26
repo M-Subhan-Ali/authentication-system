ff8897e50d59ab4aecb9c6442d24704e155395ce
M-Subhan-Ali <s8289552@gmail.com>
2025-04-26 23:03:33 +0500

    updating

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors({
     // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
     
-    if (allowedOrigins.some(allowed => {
+    if (allowedOrigin.some(allowed => {
       return origin === allowed || 
              origin.endsWith(allowed.replace('https://', ''))
     })) {
