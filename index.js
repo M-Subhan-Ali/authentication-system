6d9c30e1a29ffa2f4c33a399051cf8587a8b63af
M-Subhan-Ali <s8289552@gmail.com>
2025-04-26 23:28:34 +0500

    update bug

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ DBConnect();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://authentication-mern-app-gold.vercel.app',
+  origin: 'https://authentication-front-end-app.vercel.app',
   methods: ['GET', 'POST' , 'PUT', 'DELETE' , 'PATCH'],
   credentials: true 
 }));
